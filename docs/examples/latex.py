98860eb4d4e52cd1a1e830bef3875d791899b48f
Nikola Skoric <nskoric@gmail.com>
2010-10-14 19:11:58 +0200

    Cleaning up latex example.

--- docs/examples/latex.py
+++ docs/examples/latex.py
@@ -1,16 +1,11 @@
 import os
+import locale
 from simplepyged import *
 
 def name(person):
     if person is None:
         return ""
     return str(person.surname()) + ", " + str(person.given_name())
-
-def name_link_html(person, prefix=""):
-    return "<a href=\"#" + prefix + str(person.pointer()) + "\">" + name(person) + "</a> "
-
-def name_link_latex(person, prefix=""):
-    return name(person) + ' (str. \pageref{' + prefix + str(person.pointer()) + '})'
 
 def link_latex(element, prefix=""):
     if element == None:
@@ -22,79 +17,51 @@ def link_latex_index(element, prefix=""):
         return ""
     return '\pageref{' + prefix + str(element.pointer()) + '}'
 
-def anchor(person):
-    return "[" + str(person.pointer()) + "] "
-
-def anchor_html(person, prefix=""):
-    if person == None:
-        return "__________"
-    return "<a name=\"" + prefix + str(person.pointer()) + "\"></a> "
-
 def anchor_latex(person, prefix=""):
     return '\label{' + prefix + str(person.pointer()) + '}'
 
-def text(family):
-    retval = ""
-    retval += anchor(family.husband()) + "Husband: " + name(family.husband()) + "\n"
-    retval += anchor(family.wife()) + "Wife: " + name(family.wife()) + "\n"
-    for c in family.children():
-        retval += anchor(c) + "Child: " + name(c) + "\n"
-    retval += "\n"
-    retval += "\n"
-
-    return retval
-
 def start_latex():
     retval = ''
-    retval += '\documentstyle[12pt]{article}\\begin{document}' + "\n"
+    retval += '\documentclass[12pt]{article}' + "\n"
+    retval += '\usepackage[utf8]{inputenc}' + "\n"
+    retval += '\usepackage{multicol}' + "\n"
+    retval += '\\begin{document}' + "\n"
     return retval
 
 def latex(family):
     retval = ''
     retval += anchor_latex(family) + "\n"
-    retval += '\\begin{tabular*}{0.9\\textwidth}{ | l | l | }' + "\n"
-#    retval += '\\begin{tabular}{ l r }' + "\n"
-    retval += 'Husband '
-    retval += ' & '
-    retval += 'Wife '
-    retval += ' \\\\ ' + "\n"
-    if family.husband() is not None:
-        retval += name(family.husband()) + link_latex(family.husband().parent_family())
-    retval += ' & '
-    if family.wife() is not None:
-        retval += name(family.wife()) + link_latex(family.wife().parent_family())
-    retval += ' \\\\ ' + "\n"
-    if family.husband() is not None:
-        retval += str(family.husband().birth()[0])
-    retval += ' & '
-    if family.wife() is not None:
-        retval += str(family.wife().birth()[0])
-    retval += ' \\\\ ' + "\n"
-    if family.husband() is not None:
-        retval += str(family.husband().birth()[1])
-    retval += ' & '
-    if family.wife() is not None:
-        retval += str(family.wife().birth()[1])
-    retval += ' \\\\ ' + "\n"
-    retval += ' \\\\ ' + "\n"
-    if family.husband() is not None:
-        retval += str(family.husband().death()[0])
-    retval += ' & '
-    if family.wife() is not None:
-        retval += str(family.wife().death()[0])
-    retval += ' \\\\ ' + "\n"
-    if family.husband() is not None:
-        retval += str(family.husband().death()[1])
-    retval += ' & '
-    if family.wife() is not None:
-        retval += str(family.wife().death()[1])
-    retval += ' \\\\ ' + "\n"
-    retval += '\end{tabular*}' + "\n\n"
+    retval += '\\begin{multicols}{3}' + "\n"
+    retval += '*' + " \\\\ \n"
+    retval += 'Name: ' + " \\\\ \n"
+    retval += 'Birth date: ' + " \\\\ \n"
+    retval += 'Birth place: ' + " \\\\ \n"
+    retval += 'Death date: ' + " \\\\ \n"
+    retval += 'Death place: ' + " \\\\ \n"
+    retval += '\\columnbreak ' + " \\\\ \n"
 
-    retval += '\\paragraph{}' + "\n"
+    retval += 'Husband' + " \\\\ \n"
+    if family.husband() is not None:
+        retval += name(family.husband()) + link_latex(family.husband().parent_family()) + " \\\\ \n"
+        retval += str(family.husband().birth()[0]) + " \\\\ \n"
+        retval += str(family.husband().birth()[1]) + " \\\\ \n"
+        retval += str(family.husband().death()[0]) + " \\\\ \n"
+        retval += str(family.husband().death()[1]) + " \\\\ \n"
+    retval += '\\columnbreak ' + " \\\\ \n"
+
+    retval += 'Wife' + " \\\\ \n"
+    if family.wife() is not None:
+        retval += name(family.wife()) + link_latex(family.wife().parent_family()) + " \\\\ \n"
+        retval += str(family.wife().birth()[0]) + " \\\\ \n"
+        retval += str(family.wife().birth()[1]) + " \\\\ \n"
+        retval += str(family.wife().death()[0]) + " \\\\ \n"
+        retval += str(family.wife().death()[1]) + " \\\\ \n"
+    retval += '\end{multicols}' + "\n\n"
+
+#    retval += '\\paragraph{}' + "\n"
     retval += 'Married: ' + '' + "\n\n"
 
-    retval += '\\paragraph{}' + "\n\n"
+#    retval += '\\paragraph{}' + "\n\n"
     retval += '\\begin{center}Children\end{center}' + '' + "\n\n"
     for i in range(1, len(family.children()) + 1):
         c = family.children()[i - 1]
@@ -167,15 +134,17 @@ def latex_index(stack):
         for c in family.children():
             individuals = push(individuals, c)
 
-    individuals = sorted(individuals, key=name)
+    locale.setlocale(locale.LC_ALL, '')
 
-    retval = ""
+    individuals.sort(cmp=lambda x, y: locale.strcoll(name(x), name(y)))
+
+    retval = ''
+    retval += '\\begin{multicols}{2}' + "\n"
     for i in individuals:
         if i.given_name() == '' and i.surname() == '':
             continue
-        retval += '\\begin{tabular*}{0.9\\textwidth}{  l  r  }' + "\n"
         retval += name(i) + " "
-        retval += ' & '
+        retval += ' \hfill '
         pages = []
         if i.parent_family() is not None:
             pages = push(pages, link_latex_index(i.parent_family()))
@@ -184,10 +153,9 @@ def latex_index(stack):
                 pages = push(pages, link_latex_index(f))
 
         retval += ', '.join(pages)
-        retval += ' \\\\ ' + "\n"
+        retval += '\\newline ' + "\n"
 
-        retval += '\end{tabular*}' + "\n\n"
-
+    retval += '\\end{multicols}' + "\n"
     return retval
 
 #g = Gedcom(os.path.abspath('mcintyre.ged'))
