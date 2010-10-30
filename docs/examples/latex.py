5c457b6e082dff5d36064a06d56e556f18be5425
Nikola Skoric <nskoric@gmail.com>
2010-10-31 01:57:46 +0200

    Refactoring latex example.

--- docs/examples/latex.py
+++ docs/examples/latex.py
@@ -3,113 +3,159 @@ import os
 import locale
 from simplepyged.gedcom import *
 
-def escape_latex(in_str):
-    map = {'#': '\\#', 
-	'$': '\\$', 
-	'%': '\\%', 
-	'^': '\\textasciicircum{}', 
-	'&': '\\&', 
-	'_': '\\_', 
-	'{': '\\{', 
-	'}': '\\}', 
-	'~': '\\~{}', 
-	'\\': '\\textbackslash{}'}
+class Stack:
+    def __init__(self, stack = []):
+        self.stack = stack
+        self.sort = self.stack.sort
 
-    in_str = unicode(in_str)
+    def __contains__(self, item):
+        return item in self.stack
 
-    retval = ''
-    for c in in_str:
-        if c in map.keys():
-            c = map[c]
-        retval += c
+    def push(self, item):
+        if item == None:
+            return
 
-    return retval
-            
+        if item in self.stack:
+            return
 
-def name(person):
-    if person is None:
-        return ""
+        self.stack.append(item)
 
-    if person.surname() is None:
-        s = ''
-    else:
-        s = person.surname()
+    def empty(self):
+        return self.stack == []
 
-    if person.given_name() is None:
-        g = ''
-    else:
-        g = person.given_name()
-    return s + ", " + g
+    def __iter__(self):
+        return self.stack.__iter__()
 
-def push(stack, item):
-    if item == None:
+    def next(self):
+        return self.stack.next()
+
+class LatexReport:
+    def __init__(self, gedcom):
+        self.template = 'template.tex'
+        self.gedcom = gedcom
+
+    @staticmethod
+    def name(person):
+        """ Format person's name as you wish """
+        
+        if person is None:
+            return ""
+
+        if person.surname() is None:
+            s = ''
+        else:
+            s = person.surname()
+
+        if person.given_name() is None:
+            g = ''
+        else:
+            g = person.given_name()
+        return s + ", " + g
+
+    @staticmethod
+    def escape_latex(in_str):
+        """ Escaping LaTeX special characters """
+        
+        map = {'#': '\\#', 
+            '$': '\\$', 
+            '%': '\\%', 
+            '^': '\\textasciicircum{}', 
+            '&': '\\&', 
+            '_': '\\_', 
+            '{': '\\{', 
+            '}': '\\}', 
+            '~': '\\~{}', 
+            '\\': '\\textbackslash{}'}
+
+        in_str = unicode(in_str)
+
+        retval = ''
+        for c in in_str:
+            if c in map.keys():
+                c = map[c]
+            retval += c
+
+        return retval
+
+    def construct_stack(self, stack, depth):
+        """ Construct a stack full of depth distant relatives of members of stack (a recursion, obviously) """
+        
+        if depth == 0:
+            return []
+        if stack.empty():
+            return stack
+
+        for family in stack:
+            if family == None:
+                continue
+            addendum = Stack()
+            for p in family.parents():
+                addendum.push(p.parent_family())
+            for c in family.children():
+                for f in c.families():
+                    addendum.push(f)
+
+        expanded_family = construct_stack(addendum, depth - 1)
+
+        for e in expanded_family:
+            stack.push(e)
+
         return stack
 
-    if item in stack:
-        return stack
+    def latex_index(self, stack):
+        """ Create an index of all individuals printed in report """
+        
+        individuals = Stack()
+        for family in stack:
+            for p in family.parents():
+                individuals.push(p)
+            for c in family.children():
+                individuals.push(c)
 
-    stack.append(item)
-    return stack
+        locale.setlocale(locale.LC_ALL, '')
+        individuals.sort(cmp=lambda x, y: locale.strcoll(self.name(x), self.name(y)))
 
-def construct_stack(stack, depth):
-    if depth == 0:
-        return []
-    if stack == []:
-        return []
+        return individuals
 
-    for family in stack:
-        if family == None:
-            continue
-        addendum = []
-        for p in family.parents():
-            addendum = push(addendum, p.parent_family())
-        for c in family.children():
-            for f in c.families():
-                addendum = push(addendum, f)
+    def pages(self, individual):
+        """ Return list of xrefs of families in which a person can be found """
+        
+        pages = []
+        if individual.parent_family() is not None:
+            return [individual.parent_family().xref()] # Person's parent's family is preferable link to person
+        for f in individual.families(): # If person doesn't have parent family, then index his child families
+            if f is not None:
+                pages.append(f.xref())
 
-    expanded_family = construct_stack(addendum, depth - 1)
+        return pages
 
-    for e in expanded_family:
-        stack = push(stack, e)
+    def get_latex(self, stack = None):
+        """ Print out latex code using mako template engine """
+        
+        if stack is None:
+            stack = Stack(self.gedcom.family_list())
 
-    return stack
+        latex = Template(
+            filename = self.template,
+            default_filters=['unicode', 'escape_latex'],
+            imports=['from latex import escape_latex']) # so that mako.template.Template can find escape_latex
+        source = latex.render_unicode(
+            stack=stack.stack,
+            index=self.latex_index(stack.stack),
+            pages=self.pages,
+            ).encode('utf-8')
 
-def latex_index(stack):
-    individuals = []
-    for family in stack:
-        for p in family.parents():
-            individuals = push(individuals, p)
-        for c in family.children():
-            individuals = push(individuals, c)
+        return source
 
-    locale.setlocale(locale.LC_ALL, '')
+escape_latex = LatexReport.escape_latex # so that mako.template.Template can find escape_latex
 
-    individuals.sort(cmp=lambda x, y: locale.strcoll(name(x), name(y)))
+g = Gedcom(os.path.abspath('../../test/mcintyre.ged'))
+#g = Gedcom(os.path.abspath('../../test/wright.ged'))
+l = LatexReport(g)
 
-    return individuals
-
-def pages(individual):
-    pages = []
-    if individual.parent_family() is not None:
-        return [individual.parent_family().xref()]
-    for f in individual.families():
-        if f is not None:
-            pages.append(f.xref())
-
-    return pages
-
-#g = Gedcom(os.path.abspath('../../test/mcintyre.ged'))
-g = Gedcom(os.path.abspath('../../test/wright.ged'))
+# This is some old code that doesn't work anymore (TODO: rewrite this)
 #fam = g.get_family('@F5@')
 #stack = [fam]
 #stack = construct_stack(stack, 6)
 
-stack = g.family_list()
-
-latex = Template(
-    filename = 'template.tex',
-    default_filters=['unicode', 'escape_latex'],
-    imports=['from latex import escape_latex'])
-source = latex.render_unicode(stack=stack, index=latex_index(stack), pages=pages).encode('utf-8')
-
-print source
+print  l.get_latex()
