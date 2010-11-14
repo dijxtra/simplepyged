2b71b484a5bd82313efe52cee31b03e1269fc522
Nikola Skoric <nskoric@gmail.com>
2010-11-14 21:46:47 +0100

    Updating test cases. Now everything passes.

--- test/test_simplepyged.py
+++ test/test_simplepyged.py
@@ -9,6 +9,14 @@ class McIntyreTest(unittest.TestCase):
     def setUp(self):
         self.g = Gedcom(os.path.abspath('test/mcintyre.ged'))
 
+    def test_parser(self):
+        """Check if parser collected all records"""
+        self.assertEqual(len(self.g.record_dict()), 57)
+
+        self.assertEqual(len(self.g.individual_list()), 41)
+        self.assertEqual(len(self.g.family_list()), 16)
+              
+
     def test_missing_xref(self):
         """I don't really know what this does... (original author didn't bother to comment) """
         for e in self.g.line_list():
